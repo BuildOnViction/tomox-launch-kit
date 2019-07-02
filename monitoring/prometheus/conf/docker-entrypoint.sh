1ebd5c0e14a2f8737732f638e461665fb0738f5f
naviat <haidv.ict@gmail.com>
2019-07-02 16:08:33 +0700

    add grafana dash template

--- monitoring/prometheus/conf/docker-entrypoint.sh
+++ monitoring/prometheus/conf/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -e
 
 cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
-cat /etc/prometheus/weave-cortex.yml | \
-    sed "s@#password: <token>#@password: '$WEAVE_TOKEN'@g" > /tmp/weave-cortex.yml
 
 #JOBS=mongo-exporter:9111 redis-exporter:9112
 
@@ -25,22 +23,11 @@ cat >>/tmp/prometheus.yml <<EOF
       port: ${PORT}
 EOF
 
-cat >>/tmp/weave-cortex.yml <<EOF
-
-  - job_name: '${SERVICE}'
-    dns_sd_configs:
-    - names:
-      - 'tasks.${SERVICE}'
-      type: 'A'
-      port: ${PORT}
-EOF
-
 done
 
 fi
 
 mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
-mv /tmp/weave-cortex.yml /etc/prometheus/weave-cortex.yml
 
 set -- /bin/prometheus "$@"
 
