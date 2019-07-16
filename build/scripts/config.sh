26b82cd0fa36b272052254eefb0ae09a2d36b825
naviat <haidv.ict@gmail.com>
2019-07-16 15:07:57 +0700

    build image from custom address

--- build/scripts/config.sh
+++ build/scripts/config.sh
@@ -1,5 +1,5 @@
 readonly DOCKER_IMAGE_NAME="tomochain/tomox-sdk-ui"
-readonly DOCKER_IMAGE_TAG="1.0"
+readonly DOCKER_IMAGE_TAG="latest"
 readonly DOCKER_IMAGE_MOUNT="/mnt/data"         # Optional
 
 readonly DOCKER_LOCAL_SOURCE_DIRECTORY="tomox-sdk-ui"
